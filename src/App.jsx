3b44588c89f084798674081e105083eec7856c64
Siavash Etemadi <your-email@example.com>
2026-07-27 20:35:00 +0330

    secend commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./UI/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/card/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/menu", element: <Menu /> },
+  { path: "/cart", element: <Cart /> },
+  { path: "/order/new", element: <CreateOrder /> },
+  { path: "/order/:orderId", element: <Order /> },
+]);
+
 function App() {
-  return <div>hello vite</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
