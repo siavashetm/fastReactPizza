3a9b6a1fcec3bc65ee7d3b40bf4a4fa232a0bb96
Siavash Etemadi <your-email@example.com>
2026-09-14 22:37:41 +0330

    end3

--- src/App.jsx
+++ src/App.jsx
@@ -9,34 +9,39 @@ import Order, { loader as orderLoader } from "./features/order/Order";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Applayout from "./UI/Applayout";
 import { action as updateOrderAction } from "./features/order/UpdateOrder";
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      element: <Applayout />,
+      errorElement: <Error />,
+      children: [
+        { path: "/", element: <Home /> },
+        {
+          path: "/menu",
+          element: <Menu />,
+          loader: menuLoader,
+          errorElement: <Error />,
+        },
+        { path: "/cart", element: <Cart /> },
+        {
+          path: "/order/new",
+          element: <CreateOrder />,
+          action: creatOrderAction,
+        },
+        {
+          path: "/order/:orderId",
+          element: <Order />,
+          loader: orderLoader,
+          errorElement: <Error />,
+          action: updateOrderAction,
+        },
+      ],
+    },
+  ],
   {
-    element: <Applayout />,
-    errorElement: <Error />,
-    children: [
-      { path: "/", element: <Home /> },
-      {
-        path: "/menu",
-        element: <Menu />,
-        loader: menuLoader,
-        errorElement: <Error />,
-      },
-      { path: "/cart", element: <Cart /> },
-      {
-        path: "/order/new",
-        element: <CreateOrder />,
-        action: creatOrderAction,
-      },
-      {
-        path: "/order/:orderId",
-        element: <Order />,
-        loader: orderLoader,
-        errorElement: <Error />,
-        action: updateOrderAction,
-      },
-    ],
+    basename: "/fastReactPizza",
   },
-]);
+);
 
 function App() {
   return <RouterProvider router={router} />;
